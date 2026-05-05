ecd6c7474caa3537623ebaee5920753fb7e5cffa
Narendra-Sharma-26 <narendra.sharma.822402@gmail.com>
2026-05-06 01:30:09 +0530

    updated question 15

--- src/Q15.java
+++ src/Q15.java
@@ -1,32 +1,68 @@
 
 // 15.	Sorting an Array
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class Q15 {
 
-    public int[] arraySort(int[] array){
+    public int[] arraySort(int[] array) {
 
-        int[] sortedArray = new int[array.length];
+        int[] sortedArray = Arrays.copyOf(array, array.length);
 
-        for(int i =0; i<array.length-1; i++ ){
-            for(int j =0; j<array.length-1; j++){
-                if(array[i]<array[j])
-                    
+        for (int i = 0; i < sortedArray.length - 1; i++) {
+
+            for (int j = i + 1; j <= sortedArray.length - 1; j++) {
+
+                int temp = sortedArray[i];
+
+                if (sortedArray[i] > sortedArray[j]) {
+                    sortedArray[i] = sortedArray[j];
+                    sortedArray[j] = temp;
+                }
             }
         }
-    }   
+        return sortedArray;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the size of Array: ");
-        int size = sc.nextInt();
+        int[] array;
+        int size = 0;
 
-        int[] array = IntStream.range(0, size).map(i -> {
-            System.out.print("Enter Number "+ (i+1)+ " : ");
-            return sc.nextInt();
-        }).toArray();
+        while (true) {
+
+            try {
+                System.out.print("Enter the size of Array: ");
+                size = sc.nextInt();
+                break;
+                // array = IntStream.range(0, size).map(i -> {
+                // System.out.print("Enter Number " + (i + 1) + " : ");
+                // return sc.nextInt();
+                // }).toArray();
+                // break;
+
+            } catch (Exception e) {
+                System.out.println("Invalid Input!!!");
+                sc.nextLine();
+            }
+
+        }
+
+        while(true) {
+            try {
+                    array = IntStream.range(0, size).map(i -> {
+                        System.out.print("Enter Number " + (i + 1) + " : ");
+                        return sc.nextInt();
+                    }).toArray();
+                    break;
+                } catch (Exception e) {
+                    System.out.println("Invalid input!!! Again!!!");
+                    sc.nextLine();
+                }
+        }
 
         Q15 obj = new Q15();
 
@@ -36,7 +72,6 @@ public class Q15 {
         System.out.println();
         System.out.print("Sorted Array: ");
         IntStream.of(result).forEach(num -> System.out.print(num + " "));
-
 
     }
 }
